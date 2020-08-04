00d9095349fef028233d2f1a3984b417cc45173b
David Jovanovic <jovanovicdv@gmail.com>
2020-08-04 17:23:55 +0200

    Started http-header binding task 2. Added a new route to the producer. Added a new starting point processor which recieves the message header parts as http headers and puts them in the body header.

--- src/main/java/it/eng/idsa/businesslogic/processor/producer/ProducerParseReceivedDataProcessorHttpHeader.java
+++ src/main/java/it/eng/idsa/businesslogic/processor/producer/ProducerParseReceivedDataProcessorHttpHeader.java
@@ -7,11 +7,13 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.simple.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import de.fraunhofer.iais.eis.Message;
 import it.eng.idsa.businesslogic.service.MultipartMessageService;
@@ -35,15 +37,6 @@ public class ProducerParseReceivedDataProcessorHttpHeader implements Processor{
 	@Override
 	public void process(Exchange exchange) throws Exception {
 		
-		String contentType;
-		String forwardTo;
-		String type;
-		String issued;
-		String issuerConnector;
-		String correlationMessage;
-		String transferContract;
-		String modelVersion;
-		String id;
 		String header = null;
 		String payload = null;
 		Message message = null;
@@ -52,7 +45,7 @@ public class ProducerParseReceivedDataProcessorHttpHeader implements Processor{
 		String receivedDataBodyBinary = null;
 
 		// Get from the input "exchange"
-		Map<String, Object> receivedDataHeader = exchange.getIn().getHeaders();
+		headersParts = exchange.getIn().getHeaders();
 		receivedDataBodyBinary = exchange.getIn().getBody(String.class);
 		if (receivedDataBodyBinary == null) {
 			logger.error("Body of the received multipart message is null");
@@ -60,15 +53,11 @@ public class ProducerParseReceivedDataProcessorHttpHeader implements Processor{
 
 		}
 		try {
-			// Create headers parts
 			// Put in the header value of the application.property: application.isEnabledDapsInteraction
 			headersParts.put("Is-Enabled-Daps-Interaction", isEnabledDapsInteraction);
-			contentType = receivedDataHeader.get("Content-Type").toString();
-			headersParts.put("Content-Type", contentType);
-			forwardTo = receivedDataHeader.get("Forward-To").toString();
-			headersParts.put("Forward-To", forwardTo);
 
 			// Create multipart message parts
+			
 			String headerFromHeaders = getHeaderFromHeadersMap(headersParts);
 			header = multipartMessageService.getHeaderContentString(headerFromHeaders);
 			multipartMessageParts.put("header", header);
@@ -88,82 +77,35 @@ public class ProducerParseReceivedDataProcessorHttpHeader implements Processor{
 					RejectionMessageType.REJECTION_MESSAGE_LOCAL_ISSUES, 
 					message);
 		}
-//		try {
-			// Create headers parts
-			// Put in the header value of the application.property: application.isEnabledDapsInteraction
-//			headers.put("Is-Enabled-Daps-Interaction", isEnabledDapsInteraction);
-//			contentType = receivedDataHeader.get("Content-Type").toString();
-//			headers.put("Content-Type", contentType);
-//			forwardTo = receivedDataHeader.get("Forward-To").toString();
-//			headers.put("Forward-To", forwardTo);
-//			type = receivedDataHeader.get("type").toString();
-//			headers.put("type", type);
-//			issued = receivedDataHeader.get("issued").toString();
-//			headers.put("issued", issued);
-//			issuerConnector = receivedDataHeader.get("issuerConnector").toString();
-//			headers.put("issuerConnector", issuerConnector);
-//			correlationMessage = receivedDataHeader.get("correlationMessage").toString();
-//			headers.put("correlationMessage", correlationMessage);
-//			transferContract = receivedDataHeader.get("transferContract").toString();
-//			headers.put("transferContract", transferContract);
-//			modelVersion = receivedDataHeader.get("modelVersion").toString();
-//			headers.put("modelVersion", modelVersion);
-//			id = receivedDataHeader.get("id").toString();
-//			headers.put("id", id);
-
-			
-			
-			// Return exchange
-//			exchange.getOut().setHeaders(headers);
-
-//		} catch (Exception e) {			
-//			logger.error("Error parsing multipart message:" + e);
-//			rejectionMessageService.sendRejectionMessage(
-//					RejectionMessageType.REJECTION_MESSAGE_LOCAL_ISSUES, 
-//					message);
-//		}		
+	
 	}
 
-private String getHeaderFromHeadersMap(Map<String, Object> headers) throws JsonProcessingException {
-		
-		StringBuffer sb = new StringBuffer();
+	private String getHeaderFromHeadersMap(Map<String, Object> headers) throws JsonProcessingException {
 
-		sb.append("{" + System.lineSeparator());
-		sb.append(appendKeyAndValue("type", headers));
-		sb.append(appendKeyAndValue("issued", headers));
-		sb.append(appendKeyAndValue("issuerConnector", headers));
-		sb.append(appendKeyAndValue("correlationMessage", headers));
-		sb.append(appendKeyAndValue("transferContract", headers));
-		sb.append(appendKeyAndValue("modelVersion", headers));
-		sb.append(appendKeyAndValue("id", headers));
-		sb.append("}");
+		Map<String, String> headerAsMap = new HashMap<String, String>();
+		headerAsMap.put("@type", headers.get("IDS-Messagetype").toString());
+		headerAsMap.put("@id", headers.get("IDS-Id").toString());
+		headerAsMap.put("issued", headers.get("IDS-Issued").toString());
+		headerAsMap.put("modelVersion", headers.get("IDS-ModelVersion").toString());
+		headerAsMap.put("issuerConnector", headers.get("IDS-IssuerConnector").toString());
+		headerAsMap.put("transferContract", headers.get("IDS-TransferContract").toString());
+		headerAsMap.put("correlationMessage", headers.get("IDS-CorrelationMessage").toString());
 
-		return sb.toString();
-		
-		
+		removeMessageHeaders(headers);
+
+		String header = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(headerAsMap);
+
+		return header;
+
 	}
 
-	private String appendKeyAndValue(String key, Map<String, Object> headersMap) {
-		StringBuffer sb = new StringBuffer();
-		String lineStart = "  \"";
-		String lineEnd = "\",";
-		String spaceBetweenKeyAndValue = "\" : \"";
-		String value = headersMap.get(key).toString();
-		
-		sb.append(lineStart);
-		if (key.equals("type") || key.equals("id")) {
-			sb.append("@");
-		}
-		sb.append(key);
-		sb.append(spaceBetweenKeyAndValue);
-		sb.append(value);
-		if (!key.equals("id")) {
-			sb.append(lineEnd);
-		}else {
-			sb.append("\"");
-		}
-		sb.append(System.lineSeparator());
-		
-		return sb.toString();
+	private void removeMessageHeaders(Map<String, Object> headers) {
+		headers.remove("IDS-Messagetype");                     
+		headers.remove("IDS-Id");                                
+	    headers.remove("IDS-Issued");                         
+        headers.remove("IDS-ModelVersion");             
+        headers.remove("IDS-IssuerConnector");       
+        headers.remove("IDS-TransferContract");     
+        headers.remove("IDS-CorrelationMessage"); 
 	}
 }
